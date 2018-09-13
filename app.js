b31a17343582ea43dd0bac0b81b29c1340cb2f0a
Scott Hurlow <Scottyhurlow@gmail.com>
2018-09-13 15:57:05 -0700

    general cleanup. still broken

--- app.js
+++ app.js
@@ -2,31 +2,23 @@ var express = require('express')
 var bodyParser = require('body-parser')
 var fs = require('fs')
 var colors = require('colors')
-var haiku = require('./haiku')
-var randomWords = require('random-words');
+// var haiku = require('./haiku')
+// var randomWords = require('random-words');
 var oauth = require('oauth')
 var Oauth = new oauth.OAuth(
     'https://api.twitter.com/oauth/request_token',
     'https://api.twitter.com/oauth/access_token',
-    '4WqSd4NKuRN6h6KspsbNAHhDq',
-    'ZJt1ufogxSf66SLr1wbYiW9NLChKQVypfL8FVLNn2bvOEvfcuq',
+    '<KEY>',
+    '<SECRET>',
     '1.0',
     null,
     'HMAC-SHA1', null,
     {
       'Accept': '*/*',
       'Connection': 'close',
-      'User-Agent': 'haikutweet/0.1.0' 
+      'User-Agent': 'haikutweet/0.1.0'
     })
-// var querystring = require('querystring')
-// var util = require('util')
-// var T = require('twitter')
-// var twit = new T({
-//   consumer_key: '4WqSd4NKuRN6h6KspsbNAHhDq',
-//   consumer_secret: 'ZJt1ufogxSf66SLr1wbYiW9NLChKQVypfL8FVLNn2bvOEvfcuq',
-//   access_token_key: '2320992961-DPRXx6kiVsGt7g0bHqG014mGpEomiwXG0J5q4ok',
-//   access_token_secret: 'd5PizJ3umW4jUx1XPBN1Q4CShS5sZ8ceLCx5yS05yeHFf'
-// })
+
 var app = express()
 var port = process.env.PORT || 3001
 
@@ -39,13 +31,11 @@ app.use(bodyParser())
 
 
 app.get('/', function(req, res) {
-
   console.log('Hello from 3000')
   res.render('index.hjs')
-
 })
 
-  
+
 app.post('/keyword', function(req, res) {
 
   var query = req.body.keyword
@@ -56,8 +46,8 @@ app.post('/keyword', function(req, res) {
 
   Oauth.get(
     'https://api.twitter.com/1.1/search/tweets.json?count=20&q='+query,
-    '2320992961-DPRXx6kiVsGt7g0bHqG014mGpEomiwXG0J5q4ok',
-    'd5PizJ3umW4jUx1XPBN1Q4CShS5sZ8ceLCx5yS05yeHFf',
+    '<KEY>',
+    '<KEY>',
     function(err, data){
       if (err) {
         console.error(err)
@@ -69,46 +59,9 @@ app.post('/keyword', function(req, res) {
         })
       }
     })
-
-  // var query = 'pizza'
-  // twit.get('https://api.twitter.com/1.1/search/tweets.json', { q: query } , function(data, response) {
-  //     console.log(response.statusCode)
-  //     tweets = data
-  //     console.log(data)
-
-
-
-  //     // findHaiku
-
-  //     // if (tweets != undefined){
-  //     //   haiku = findHaiku(tweets, function(H) {
-  //     //     res.send(H)
-  //     //   })
-  //     // }
-  // })
 })
 
-app.post('/random', function(req, res) {
-  var word = randomWords(1)
-  console.log(word)
 
-  Oauth.get(
-    'https://api.twitter.com/1.1/search/tweets.json?count=20&q='+word,
-    '2320992961-DPRXx6kiVsGt7g0bHqG014mGpEomiwXG0J5q4ok',
-    'd5PizJ3umW4jUx1XPBN1Q4CShS5sZ8ceLCx5yS05yeHFf',
-    function(err, data){
-      if (err) {
-        console.error(err)
-        res.send(err)
-      } else {
-        findHaiku(data, word, function(H) {
-          res.send(H)
-        }) 
-      }
-
-    })
-
-})
 
 
 // Given an array of sentence strings, findHaiku() looks for
@@ -122,13 +75,13 @@ function findHaiku(data, query, cb) {
 
   // for (var i = 0; i <= count - 1; i++) {
   while (foundHaiku == false) {
-    
+
     if (i >= numTweets) {
       console.log('--possible re-poll oportunity--')
       cb({"line1":"Could not find haiku...", "line2":"", "line3":""})
       break
     }
-    
+
     str = tweets[i].text
     haiku(str, function(err, data){
       if (err) {
