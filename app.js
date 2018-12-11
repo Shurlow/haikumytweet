23cb9f17caa6ddccd0d3bd9fd2661c5cadad35df
Scott Hurlow <Scottyhurlow@gmail.com>
2018-12-11 15:32:25 -0800

    getting tweets with search query

--- app.js
+++ app.js
@@ -30,27 +30,19 @@ app.use(morgan('dev'))
 //       'User-Agent': 'haikutweet/0.1.0'
 //     })
 
-const API_KEY = 'tUP9uIJDSIOpj4fS2ujOsb2ba'
-const API_SECRET = 'VFmfHaete3JTTjPc2sdU83jF1rMqMO9MvylW0RnzNjPrwDGHrd'
 
 app.get('/', function(req, res) {
   const search = req.query.search
 
   if (!search) return res.send('Welcome to Haiku My Tweet!')
 
-  axios.get({
-    url: 'https://api.twitter.com/1.1/tweets/search/30day/Prod.json',
-    headers: { 'Authorization': "bearer " + token },
-    data: {
-      query: "keyword:pizza lang:en",
-      maxResults: 3,
-      // fromDate: "<YYYYMMDDHHmm>",
-      // toDate: "<YYYYMMDDHHmm>"
-    }
+  axios.get('https://api.twitter.com/1.1/search/tweets.json?q=pizza', {
+    headers: { 'Authorization': "bearer " + token }
   }).then((result) => {
     console.log(result);
     
   })
+  .catch(console.error)
 
     
 })
