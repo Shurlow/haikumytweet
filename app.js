6f1b55138b9b7542216af19f3d48160bc8fee5bd
Scott Hurlow <Scottyhurlow@gmail.com>
2018-12-12 13:36:07 -0800

    working get request to twitter api

--- app.js
+++ app.js
@@ -4,33 +4,11 @@ const bodyParser = require('body-parser')
 const morgan = require("morgan");
 const axios = require('axios')
 require('dotenv').config()
-
 const port = process.env.PORT || 3001
-
 const app = express()
-// app.set('views', __dirname + '/public')
-// app.set('view engine', 'hjs')
-// app.use(express.static(__dirname + '/public'))
-// app.use(bodyParser())
+
 app.use(bodyParser.json());
 app.use(morgan('dev'))
-
-// var haiku = require('./haiku')
-// var randomWords = require('random-words');
-// var oauth = require('oauth')
-// var Oauth = new oauth.OAuth(
-//     'https://api.twitter.com/oauth/request_token',
-//     'https://api.twitter.com/oauth/access_token',
-//     '<KEY>',
-//     '<SECRET>',
-//     '1.0',
-//     null,
-//     'HMAC-SHA1', null,
-//     {
-//       'Accept': '*/*',
-//       'Connection': 'close',
-//       'User-Agent': 'haikutweet/0.1.0'
-//     })
 
 const token = process.env.TWITTER_TOKEN
 
@@ -43,14 +21,13 @@ app.get('/', function(req, res) {
     headers: { 'Authorization': "bearer " + token }
   }).then((result) => {
     console.log(result);
-    
+    res.send(200)
   })
   .catch(console.error)
-
     
 })
 
-app.use('/search', require('./routes.js'))
+// app.use('/search', require('./routes.js'))
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
